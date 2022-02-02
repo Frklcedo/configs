658ea3127a8a50f167d603c696f58bdce90254b3
Frklcedo <franklinmacedo5115@gmail.com>
2022-02-01 22:17:28 -0300

    awesome layout

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -63,8 +63,8 @@ modkey = "Mod4"
 awful.layout.layouts = {
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
-    awful.layout.suit.floating,
     awful.layout.suit.max,
+    awful.layout.suit.floating,
     --awful.layout.suit.tile.bottom,
     awful.layout.suit.tile.top,
     awful.layout.suit.fair,
